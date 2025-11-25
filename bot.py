31d8771a02abab8feb263f686e8c57b973bfc1f4
Shoxjaxon1202 <shoxjaxonabdusalimov1202@gmail.com>
2025-11-25 19:02:06 +0500

    bot.py qo'shildi

---
+++ bot.py
@@ -0,0 +1,18 @@
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
+
+bot = AsyncTeleBot('8452830462:AAHwDi9bQnAKerfWPwijayKMlrQUqSrRQ-o')
+
+
+@bot.message_handler(commands=['help', 'start'])
+async def send_welcome(message):
+    text = 'Hi, I am EchoBot.\nJust write me something and I will repeat it!'
+    await bot.reply_to(message, text)
+
+
+@bot.message_handler(func=lambda message: True)
+async def echo_message(message):
+    await bot.reply_to(message, message.text)
+
+
+asyncio.run(bot.polling())
